e0e50af56eefa8cc303628546d30073c38c9f2d8
SRIKANTHRGM <kanthsri2845@gmail.com>
2024-11-23 09:52:52 +0530

    Add files via upload

---
+++ html project/king.js
@@ -0,0 +1,25 @@
+<html>
+<head>
+    
+    <title>Document</title>
+    <script>
+        function add(){
+            var a,b,c;
+            a=document.getElementById(x).value;
+            b=document.getElementById(y).value;
+            c=parseInt(a) +parseInt(b);
+           
+         
+         
+        
+        window.alert("value of c is :"+ c);
+        
+</head>
+<body>
+    first number<input type ="text" Id="x">
+    second number<input type ="text" Id="x">
+    <input type="submit" onclick="add()">
+
+
+</body>
+</html>
